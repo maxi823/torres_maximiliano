cf88719b9f3a36d88a3a9bee6e2ea045f10ccac4
maxi823 <maximiliano.torresal@gmail.com>
2025-05-05 19:26:20 -0300

    Update info.js

--- Semana_6/info.js
+++ Semana_6/info.js
@@ -1,7 +1,6 @@
- # Curso de programación full stack
 
 ## MÓDULO 2 - Repositorio y WorkFlow
 
 **Autor:** Maximiliano Torres
 
-**Descripción:** Este repositorio fue creado con fines académicos por lo que en el futuro cercano será eliminado.
+**Descripción:** Esto es una tarea de repositorio
